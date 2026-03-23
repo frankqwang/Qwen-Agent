de14c338861cdab7264a33949ca8bd88075f0f8e
frank.wang <frank.wang@temu.com>
2026-03-23 22:10:37 +0800

    Add STS2 autoplay agent example

--- qwen_agent/agents/__init__.py
+++ qwen_agent/agents/__init__.py
@@ -1,16 +1,19 @@
 # Copyright 2023 The Qwen team, Alibaba Group. All rights reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import importlib
+from typing import Iterable
 
 from qwen_agent.agent import Agent, BasicAgent
 from qwen_agent.multi_agent_hub import MultiAgentHub
@@ -19,8 +22,6 @@ from .article_agent import ArticleAgent
 from .assistant import Assistant
 from .dialogue_retrieval_agent import DialogueRetrievalAgent
 from .dialogue_simulator import DialogueSimulator
-# DocQAAgent is the default solution for long document question answering.
-# The actual implementation of DocQAAgent may change with every release.
 from .doc_qa import BasicDocQA as DocQAAgent
 from .doc_qa import ParallelDocQA
 from .fncall_agent import FnCallAgent
@@ -30,7 +31,6 @@ from .group_chat_creator import GroupChatCreator
 from .human_simulator import HumanSimulator
 from .react_chat import ReActChat
 from .router import Router
-from .tir_agent import TIRMathAgent
 from .user_agent import UserAgent
 from .virtual_memory_agent import VirtualMemoryAgent
 from .write_from_scratch import WriteFromScratch
@@ -55,5 +55,18 @@ __all__ = [
     'FnCallAgent',
     'VirtualMemoryAgent',
     'DialogueRetrievalAgent',
-    'TIRMathAgent',
 ]
+
+
+def _safe_import(module_name: str, exported_names: Iterable[str]) -> None:
+    try:
+        module = importlib.import_module(f'.{module_name}', __name__)
+    except ImportError:
+        return
+
+    for name in exported_names:
+        globals()[name] = getattr(module, name)
+        __all__.append(name)
+
+
+_safe_import('tir_agent', ['TIRMathAgent'])
--- qwen_agent/tools/__init__.py
+++ qwen_agent/tools/__init__.py
@@ -1,53 +1,52 @@
 # Copyright 2023 The Qwen team, Alibaba Group. All rights reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .amap_weather import AmapWeather
+import importlib
+from typing import Iterable
+
 from .base import TOOL_REGISTRY, BaseTool
-from .code_interpreter import CodeInterpreter
-from .doc_parser import DocParser
-from .extract_doc_vocabulary import ExtractDocVocabulary
-from .image_gen import ImageGen
-from .python_executor import PythonExecutor
-from .retrieval import Retrieval
-from .image_zoom_in_qwen3vl import ImageZoomInToolQwen3VL
-from .image_search import ImageSearch
-from .search_tools import FrontPageSearch, HybridSearch, KeywordSearch, VectorSearch
-from .simple_doc_parser import SimpleDocParser
-from .storage import Storage
-from .web_extractor import WebExtractor
 from .mcp_manager import MCPManager
-from .web_search import WebSearch
 
 __all__ = [
     'BaseTool',
-    'CodeInterpreter',
-    'ImageGen',
-    'AmapWeather',
     'TOOL_REGISTRY',
-    'DocParser',
-    'KeywordSearch',
-    'Storage',
-    'Retrieval',
-    'ImageZoomInToolQwen3VL',
-    'ImageSearch',
-    'WebExtractor',
-    'SimpleDocParser',
-    'VectorSearch',
-    'HybridSearch',
-    'FrontPageSearch',
-    'ExtractDocVocabulary',
-    'PythonExecutor',
     'MCPManager',
-    'WebSearch',
 ]
+
+
+def _safe_import(module_name: str, exported_names: Iterable[str]) -> None:
+    try:
+        module = importlib.import_module(f'.{module_name}', __name__)
+    except ImportError:
+        return
+
+    for name in exported_names:
+        globals()[name] = getattr(module, name)
+        __all__.append(name)
+
+
+_safe_import('amap_weather', ['AmapWeather'])
+_safe_import('code_interpreter', ['CodeInterpreter'])
+_safe_import('doc_parser', ['DocParser'])
+_safe_import('extract_doc_vocabulary', ['ExtractDocVocabulary'])
+_safe_import('image_gen', ['ImageGen'])
+_safe_import('python_executor', ['PythonExecutor'])
+_safe_import('retrieval', ['Retrieval'])
+_safe_import('image_zoom_in_qwen3vl', ['ImageZoomInToolQwen3VL'])
+_safe_import('image_search', ['ImageSearch'])
+_safe_import('search_tools', ['FrontPageSearch', 'HybridSearch', 'KeywordSearch', 'VectorSearch'])
+_safe_import('simple_doc_parser', ['SimpleDocParser'])
+_safe_import('storage', ['Storage'])
+_safe_import('web_extractor', ['WebExtractor'])
+_safe_import('web_search', ['WebSearch'])
